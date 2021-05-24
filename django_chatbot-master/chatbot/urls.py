a199a64420710ad53419a730194d033572d0b613
maatanyy <maatanyy@naver.com>
2021-05-25 00:11:16 +0900

    1

---
+++ django_chatbot-master/chatbot/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import url, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.start, name="start"),
+    path('home/', views.home, name="home"),
+    path('home/chattrain', views.chattrain, name="chattrain"),
+    path('home/chatanswer', views.chatanswer, name="chatanswer"),
+]
