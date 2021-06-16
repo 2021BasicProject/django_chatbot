f750657f2fddfc06d17f1a7f7b0347c1ce91f783
maatanyy <76435279+maatanyy@users.noreply.github.com>
2021-06-16 17:30:59 +0900

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 
 from . import views
 
+# 웹 사용위한 path 추가 노민성 정종현
 urlpatterns = [
     path('', views.start, name="start"),
     path('home/', views.home, name="home"),
